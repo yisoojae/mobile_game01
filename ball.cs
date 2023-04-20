e978111ec719ef3fff5defd3ce2075dc9480daf1
ysj <captnwo@naver.com>
2023-04-20 19:22:14 +0900

    bird 움직임 추가

--- ball.cs
+++ ball.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class ball : MonoBehaviour
 {
     // Start is called before the first frame update
-    float speed, n;
+    float speed, times, height, timer;
     void Start()
     {
-        speed = Random.Range(0, 9) / 4f + 2f; n = 1;
+        speed = Random.Range(0, 9) / 4f + 2f; times = 1;
+        height = Random.Range(0, 10);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        this.transform.position = new Vector2(this.transform.position.x + speed / 45 * n, this.transform.position.y);
+        if (height < 5) this.transform.position = new Vector2(this.transform.position.x + speed / 45 * times, this.transform.position.y + 0.05f);
+        else this.transform.position = new Vector2(this.transform.position.x + speed / 45 * times, this.transform.position.y - 0.05f);
         if (this.transform.position.x > 5) Destroy(this.gameObject);
+        if (timer + 0.1f < Time.time)
+        {
+            if (height == 9) height = 0;
+            else height++;
+            timer = Time.time;
+        }
     }
 }
