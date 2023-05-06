8cb371a5d486d9d238becf7de6ad54db74702ba3
YiSooJae <captnwo@naver.com>
2023-05-06 12:22:21 +0900

    배경음악 랜덤형식으로 변경

--- sound.cs
+++ sound.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class sound : MonoBehaviour
 {
+    public AudioClip[] audio00;
+    byte musicN, tmp_int;
     // Start is called before the first frame update
     void Start()
     {
-        
+        musicN = (byte)Random.Range(0, audio00.Length);
+        GetComponent<AudioSource>().clip = audio00[musicN];
+        GetComponent<AudioSource>().Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!GetComponent<AudioSource>().isPlaying)
+        {
+            tmp_int = (byte)Random.Range(0, audio00.Length);
+            if (tmp_int == musicN) tmp_int++;
+            musicN = tmp_int;
+            GetComponent<AudioSource>().clip = audio00[musicN];
+            GetComponent<AudioSource>().Play();
+        }
     }
 }
